76bb8221d45bb8ee9b178bd7583e9355dac08c86
Tiebienotjuh <74455765+Tiebienotjuh@users.noreply.github.com>
2020-11-24 21:09:42 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const RPC = require("discord-rpc");
+const rpc = new RPC.Client({
+    transport: "ipc"
+});
+
+rpc.on("ready", () => {
+    rpc.setActivity({
+        details: "Egghosting.com/discord",
+        state: "Working as CEO",
+        startTimestamp: new Date(),
+        largeImageKey: "egghrpc",
+        largeImageText: "egghosting.com",
+        smallImageKey: "dci",
+        SmallImageText: "egghosting.com"
+    });
+
+    console.log("Egghosting Rich Precense is nu geactiveerd! Kusjes Tiebe <3")
+});
+
+rpc.login({
+    clientId: "780840031131795537"
+});
